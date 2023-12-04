53c37c1e55aca5f39fb3ea83f17a0f1bd74b492a
Tyler Nijmeh <tylernij@gmail.com>
2023-12-04 11:03:27 -0800

    Send msg with sig separately
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- comms.py
+++ comms.py
@@ -1,16 +1,23 @@
 # Used to simulate data being sent online,
 # but really it's just a text file.
 
-COMMS_FILE = 'Transmitted_Data'
+COMMS_MSG_FILE = 'Transmitted_MSG_Data'
+COMMS_SIG_FILE = 'Transmitted_SIG_Data'
 
 
-def send(data):
+def send(data, sig):
     # Send data to the receiver.
-    with open(COMMS_FILE, 'wb') as f:
+    with open(COMMS_MSG_FILE, 'wb') as f:
         f.write(data)
+    with open(COMMS_SIG_FILE, 'wb') as f:
+        f.write(sig)
 
 
 def read():
     # Read data from the sender.
-    with open(COMMS_FILE, 'rb') as f:
-        return f.read()
+    with open(COMMS_MSG_FILE, 'rb') as f:
+        msg = f.read()
+    with open(COMMS_SIG_FILE, 'rb') as f:
+        sig = f.read()
+
+    return msg, sig
