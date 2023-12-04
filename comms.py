0b3a7e05b38ac08703a7e009359562e37fee5d88
Tyler Nijmeh <tylernij@gmail.com>
2023-12-04 11:15:40 -0800

    Effectively encrypt data with sig and digest
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- comms.py
+++ comms.py
@@ -3,14 +3,17 @@
 
 COMMS_MSG_FILE = 'Transmitted_MSG_Data'
 COMMS_SIG_FILE = 'Transmitted_SIG_Data'
+COMMS_DIG_FILE = 'Transmitted_DIG_Data'
 
 
-def send(data, sig):
+def send(data, sig, digest):
     # Send data to the receiver.
     with open(COMMS_MSG_FILE, 'wb') as f:
         f.write(data)
     with open(COMMS_SIG_FILE, 'wb') as f:
         f.write(sig)
+    with open(COMMS_DIG_FILE, 'w') as f:
+        f.write(digest)
 
 
 def read():
@@ -19,5 +22,7 @@ def read():
         msg = f.read()
     with open(COMMS_SIG_FILE, 'rb') as f:
         sig = f.read()
+    with open(COMMS_DIG_FILE, 'r') as f:
+        digest = f.read()
 
-    return msg, sig
+    return msg, sig, digest
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Util.Padding import pad, unpad
+from Crypto.Hash import HMAC, SHA256
 import base64
 
 
@@ -33,19 +34,34 @@ class Person:
         self.other_pub_key = other_person.pub_key
 
     def send_file(self, filename):
+        # Encrypt a message and send RSA and MAC.
         with open(filename, 'rb') as f:
             message = f.read()
 
+        # AES using CBC mode with predefined IV.
         aes = AES.new(self.aes_key, AES.MODE_CBC, b'1234567890123456')
-        padded_message = pad(message, AES.block_size)
-        enc_message = aes.encrypt(padded_message)
-        rsa = PKCS1_OAEP.new(self.pub_key)
-        enc_aes_key = rsa.encrypt(self.aes_key)
 
-        comms.send(enc_message, enc_aes_key)
-        # comms.send(enc_aes_key)
+        # Pad message to 16-byte block size.
+        padded_message = pad(message, AES.block_size)
+
+        # Encrypt original message with AES.
+        enc_message = aes.encrypt(padded_message)
+
+        # Create RSA cipher using public key.
+        rsa = PKCS1_OAEP.new(self.pub_key)
+
+        # Encrypt AES key with RSA public key for signature.
+        signature = rsa.encrypt(self.aes_key)
+
+        # Generate SHA256 Hashed-MAC for the message.
+        hmac = HMAC.new(self.aes_key, digestmod=SHA256)
+        hmac.update(enc_message)
+
+        # Send to virtual sockets.
+        comms.send(enc_message, signature, hmac.hexdigest())
 
     def decrypt(self):
+        enc_message, signature, hmac_hexdigest = comms.read()
         # TODO: do a comms.read() to get the enc message and dec it
         print()
 
@@ -56,3 +72,4 @@ sender.assign_other(receiver)
 receiver.assign_other(sender)
 
 sender.send_file('input.txt')
+receiver.decrypt()
