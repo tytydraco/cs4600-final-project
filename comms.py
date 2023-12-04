63215e9e913130f6ca28b395c70fc95902970f6c
Tyler Nijmeh <tylernij@gmail.com>
2023-12-04 10:50:57 -0800

    Get basic encryption mostly working
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- comms.py
+++ comms.py
@@ -6,11 +6,11 @@ COMMS_FILE = 'Transmitted_Data'
 
 def send(data):
     # Send data to the receiver.
-    with open(COMMS_FILE, 'w') as f:
+    with open(COMMS_FILE, 'wb') as f:
         f.write(data)
 
 
 def read():
     # Read data from the sender.
-    with open(COMMS_FILE, 'r') as f:
+    with open(COMMS_FILE, 'rb') as f:
         return f.read()
--- main.py
+++ main.py
@@ -5,6 +5,10 @@
 
 import comms
 from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import PKCS1_OAEP
+import base64
 
 
 class Person:
@@ -24,8 +28,27 @@ class Person:
         # Sets the public key for the other communicator.
         self.other_pub_key = other_person.pub_key
 
+    def send_file(self, filename):
+        with open(filename, 'rb') as f:
+            message = f.read()
+
+        aes_key = get_random_bytes(16)
+        aes = AES.new(aes_key, AES.MODE_EAX)
+        enc_message = aes.encrypt(message)
+        rsa = PKCS1_OAEP.new(self.pub_key)
+        enc_aes_key = rsa.encrypt(aes_key)
+
+        comms.send(enc_message)
+        # comms.send(enc_aes_key)
+
+    def decrypt(self):
+        # TODO: do a comms.read() to get the enc message and dec it
+        print()
+
 
 sender = Person()
 receiver = Person()
 sender.assign_other(receiver)
 receiver.assign_other(sender)
+
+sender.send_file('input.txt')
