1577bd352dd67787116643ab14bec3ff602f5fc0
Tyler Nijmeh <tylernij@gmail.com>
2023-12-04 10:30:05 -0800

    Create basic class for person
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- main.py
+++ main.py
@@ -4,3 +4,22 @@
 # Final project
 
 import comms
+from Crypto.PublicKey import RSA
+
+
+class Person:
+    # A person owns an RSA key pair.
+    pub_key = None
+    priv_key = None
+
+    # And knows the other person's public key.
+    other_pub_key = None
+
+    def __init__(self):
+        # Generate an RSA keypair automatically.
+        self.priv_key = RSA.generate(2048)
+        self.pub_key = self.priv_key.public_key()
+
+    def assign_other(self, other_person):
+        # Sets the public key for the other communicator.
+        self.other_pub_key = other_person.pub_key
